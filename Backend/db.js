fe089a7290d2830637ead133476f2924168caaee
pandit <dhamdherepandit@gmail.com>
2022-11-06 21:10:32 +0530

    trying to connect to mongo

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,9 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 const mongoURI = "mongodb://localhost:27017/"
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, ()=> {
+    mongoose.connect(mongoURI, ()=>{
         console.log("connected to mongo succesfully")
     })
 }
