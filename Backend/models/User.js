f7d58cdf07fb1c5b466df6840c1f455dd3bd5883
pandit <dhamdherepandit@gmail.com>
2022-11-08 09:39:18 +0530

    authjs and notes js added

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new Schema({
-    name:{
+    name: {
         type: String,
         required: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true
     },
-    password:{
+    password: {
         type: String,
         required: true
     },
-    date:{
+    date: {
         type: Date,
         default: Date.now
     }
-  });
+});
+
+module.exports = mongoose.model('user', UserSchema);
