f81563f1a10083e7ff8294fd7b922ed25c64e404
pandit <dhamdherepandit@gmail.com>
2022-11-09 10:39:57 +0530

    mangoose added

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const user = require ('../models/User');
 
 
 // Create a user using: POSt "/api/auth". doesnt require auth
 router.get('/', (req, res) => {
     console.log(req.body);
+    const user = User(req.body);
+    user.save()
     res.send(req.body);
 })
 
