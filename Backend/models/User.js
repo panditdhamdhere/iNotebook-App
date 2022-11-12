ab60642c1f6b460f555bb44b8c8d423e6fda6803
pandit <dhamdherepandit@gmail.com>
2022-11-12 09:16:52 +0530

    added schema

---
+++ Backend/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
+
+    date:{
+        type: date,
+        default: date.now
+    }
+  });
+
+  module.exports = mongoose.model('user, userSchema');
